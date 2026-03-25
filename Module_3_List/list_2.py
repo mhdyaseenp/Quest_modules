1bcc455de1002c9747e15b358be2d63130d3c544
Muhammed Yaseen <muhammedyaseenporoli@gmail.com>
2026-03-25 11:14:14 +0530

    update 25 MAr 26

---
+++ Module_3_List/list_2.py
@@ -0,0 +1,154 @@
+""" Matrix""" 
+
+# matrix = [[0,1,2],[3,4,5],[6,7,8]]
+# for s in matrix:
+#     for m in s:     
+#         print(m)
+
+
+
+"""append()"""                          # Last index il value add cheyyaan
+
+# sample = [1,2,3,4,5,6,7,8,9,100]
+# # print(dir( sample ))
+# sample.append(2004)
+# sample.append("Jasil")
+# sample.append([200,300,400])
+
+# print(sample)
+
+
+"""Insert()"""                          # add value in specific place
+
+# num = [1,2,3,4,5,6,7,8,9]
+# num.insert(1,'jasil')
+# print(num)
+
+
+"""pop()"""                            # Remove a element
+
+# num = [1,2,3,4,5,6,7,8,9]
+# num.pop()
+# print(num)
+
+
+# remove value in index base 
+
+# num = [1,2,3,4,5,6,7,8,9]
+# num.pop(0)
+# print(num)
+
+
+# remove value in neg index 
+
+# num = [1,2,3,4,5,6,7,8,9]
+# num.pop(-1)
+# print(num)
+
+
+"""remove()"""
+# num = [1,200,3,4,5,6,7,8,9]
+# num.remove(200)
+# print(num)
+
+
+'''clear()  '''                      # clear a list ,empty list
+# num = [1,200,3,4,5,6,7,8,9]
+# num.clear()
+# print(num)
+
+
+'''count()'''                       # count cheyyaan . eg :- 200 ethra thavana undennn
+# num = [1,200,3,4,200,5,6,7,8,9]
+# print(num.count(200))
+
+
+'''Copy()'''
+# num = [1,200,3,4,5,6,7,8,9,[100,200,7]]
+# copy_list = num.copy()
+
+# copy_list[-1][-1]=700
+# print(copy_list)                                    # [1, 200, 3, 4, 5, 6, 7, 8, 9, [100, 200, 700]]
+# print(num)                                          # [1, 200, 3, 4, 5, 6, 7, 8, 9, [100, 200, 700]]
+
+
+'''sort()'''                                        # accending order ( 1 2 3 4 5 6 )
+# Accesing
+# list1 = [1, 200, 9, 7, 5, 0, 3]
+# list1.sort()
+# print(list1)                                        # [0, 1, 3, 5, 7, 9, 200]
+
+# Deccending
+# list1.sort(reverse=True)
+# print(list1)                                        # [200, 9, 7, 5, 3, 1, 0]
+
+
+# Sort in alphabetic order 
+# name = ['my', 'name', 'is', 'jasil', 'adivaram']
+# name.sort()
+# print(name)                                             # ['adivaram', 'is', 'jasil', 'my', 'name']
+
+
+# Length vechh 
+# name = ['myyyyyyyyyyyyy', 'name', 'is', 'jasil', 'adivaram']
+# name.sort(key=len)
+# print(name)
+
+
+'''Extend()'''              #to add multiple element
+
+# list1 = [1, 200, 9, 7, 5, 0, 3]
+# list2 = ['jasil',2004,'adivaram']
+# list1.extend(list2)
+# print(list1)                               # [1, 200, 9, 7, 5, 0, 3, 'jasil', 2004, 'adivaram']
+
+# list1.extend('Muhammed')
+# print(list1)                              # [1, 200, 9, 7, 5, 0, 3, 'jasil', 2004, 'adivaram', 'M', 'u', 'h', 'a', 'm', 'm', 'e', 'd']
+
+
+
+'''index()'''                                # (value,start,stop)
+# list1 = [1, 200, 9, 7, 5, 0, 3]
+# print(list1.index(200))                      # 1
+# print(list1.index(9,0,3))                    # 2
+
+
+
+'''reverse()'''
+# list1 = [1, 200, 9,'jasil', 7, 5, 0, 3]
+# list1.reverse()
+# print(list1)                                # [3, 0, 5, 7, 'jasil', 9, 200, 1]
+
+
+# list1 = [3, 0, 5, 7, 9, 200, 1]
+# # len
+# print(len(list1))                                             # 7
+
+# # min
+# print(min(list1))                                             # 0
+
+# # max
+# print(max(list1))                                             # 200
+
+# # sum
+# print(sum(list1))                                             # 225
+ 
+# # sorted
+# print(sorted(list1))                                          # [0, 1, 3, 5, 7, 9, 200]
+
+
+
+
+# nums = [1, 2, 3, 401, 10, 4008, 20]
+# large = 0
+# s_large = 0
+
+# for i in range(len(nums)):
+#     if nums[i] > large:
+#         s_large = large
+#         large = nums[i]
+#     elif large > nums[i] > s_large:
+#         s_large = nums[i]
+
+# print("Largest =", large)
+# print("Second Largest =", s_large)
--- Module_3_List/work_11_lists.py
+++ Module_3_List/work_11_lists.py
@@ -24,72 +24,268 @@
 # print("Miinimum value :",minimum)
 
 
-# 4. Write a program to append a new element to a list entered by the user.
+# 4. Write a program to append a new element to a list entered by the user
+# s=[1,2,3,4,5,6]
+# y=int(input("Enter a number :"))
+# s.append(y)
+# print(s)
 
 # 5. Insert an element at a specific position in a list.
+# s=[1,2,3,4,5]
+# y=int(input("Enter a position :"))
+# z=input("Enter a vlaue :")
+# s.insert(y,z)
+# print(s)
+
+
 # 6. Remove an element from a list using remove() and pop().
+# list1=[1,2,3,4,5,6]
+# # y=list1.pop()
+# list1.remove(5)
+# print(list1)
+
+
+
 # 7. Write a program to check whether a given element exists in a list.
+# list=[1,2,3,4,5,6]
+# y=int(input("enter the number :"))
+# if y in list:
+#     print ("its exixst",list)
+# else:
+#     print ("dosnot exixst",list)
+
+
 # 8. Reverse a list without using reverse().
+# list=[1,2,3,4,5,6]
+# sl=list[::-1]
+# print(sl)
+
+
 # 9. Sort a list of numbers in ascending and descending order.
+# list=[3,5,2,6,4,1]
+# list.sort()
+# print("Assending",list)
+# list.sort(reverse=True)
+# print("Desending",list)
+
+
 # 10. Create a list of numbers and print only the even numbers.
+# s=[2,3,5,35,6,8,45,10]
+# for i in s:
+#     if i%2==0:
+#         print(i,end=" ")
+
+
 #11. Count how many times a specific element appears in a list.
+# s=[2,3,5,35,6,8,45,6,6]
+# x=int(input("Enter the searching number :"))
+# cont=s.count(x)
+# print(cont)
+
+
 #12. Write a program to copy one list into another list.
+# list1=[1,2,3,4,5]
+# list2=list1.copy()
+# print("orginal :",list1)
+# print("copy :",list2)
+
+
 #13. Concatenate two lists using the + operator.
+# list1=[1,2,3]
+# list2=[4,5,6]
+# result=list1+list2
+# print(result)
+
 #14. Repeat a list three times using the * operator.
+# list1=[1,2,3]
+# result=list1*3
+# print(result)
+
 #15. Demonstrate positive and negative indexing in a list.
+# nums = [10, 20, 30, 40, 50]
+
+# # Positive indexing
+# print("Positive Indexing:")
+# print(nums[0])   # first element
+# print(nums[2])   # third element
+
+# # Negative indexing
+# print("\nNegative Indexing:")
+# print(nums[-1])  # last element
+# print(nums[-3])  # third from end
 
 """" Section 2: Intermediate Level"""
 # 16. Write a program to remove duplicates from a list.
-# 2 17. Find the second largest element in a list.
-# 3 18. Write a program to rotate a list to the left by one position.
-# 4 19. Write a program to rotate a list to the right by one position.
-# 5 20. Move a specific element (e.g., 50) to the first position of a list.
-# 6 21. Create a list of squares of numbers from 1–10 using list comprehension.
-# 7 22. Create a list containing only odd numbers from 1–50 using list comprehension.
-# 8 23. Write a program to merge two lists and remove duplicates.
-# 9 24. Find the sum of all elements in a list without using sum().
-# 10 25. Write a program to find common elements between two lists.
-# 11 26. Write a program to split a list into two halves.
-# 12 27. Find the index of a given element without using index().
-# 13 28. Write a program to flatten a nested list.
-# 14 29. Create a program to find the frequency of each element in a list.
-# 15 30. Reverse each element of a list of strings.
+# s=[1,2,3,4,5,5,5,66]
+# z=[]
+# for i in s:
+#    if i not in z:
+#         z.append(i)
+# print(z)
+
+
+# 17. Find the second largest element in a list.
+# s=[1,2,33,4,5,5,5,66]
+# s.sort()
+# print(s[-2])
+
+
+# 18. Write a program to rotate a list to the left by one position.
+# num=[1,2,3,4,5]
+# nums=num[1::]+[num[0]]
+# print(nums)
+
+
+# 19. Write a program to rotate a list to the right by one position.
+# num=[1,2,3,4,5]
+# nums=[num[-1]]+num[:-1]
+# print(nums)
+
+
+# 20. Move a specific element (e.g., 50) to the first position of a list.
+# num=[10,20,30,40,50,60]
+# num.remove(50)
+# num.insert(0,50)
+# print(num)
+
+
+# 21. Create a list of squares of numbers from 1–10 using list comprehension.
+# square=[]
+# for i in range(1,11):
+#     square.append(i**2)
+# print(square)
+
+
+# 22. Create a list containing only odd numbers from 1–50 using list comprehension.
+# odd=[]
+# for i in range(1,50):
+#     if i %2==1:
+#         odd.append(i)
+# print(odd)
+
+
+# 23. Write a program to merge two lists and remove duplicates.
+# list1=[1,2,34,5,2,42,4]
+# list2=[4,6,4,3,9,50,1,2]
+# list3=list1+list2
+
+# result=[]
+# for i in list3:
+#     if i not in result:
+#         result.append(i)
+# print(result)
+
+
+# 24. Find the sum of all elements in a list without using sum().
+# num=[1,2,3]
+# total=0
+# for i in num:
+#     total+=i
+# print(total)
+
+
+# 25. Write a program to find common elements between two lists.
+# list1 = [1, 2, 3, 4, 5]
+# list2 = [3, 4, 5, 6, 7]
+# common = []
+# for i in list1:
+#     if i in list2 and i not in common:
+#         common.append(i)
+# print(common)
+
+
+
+# 26. Write a program to split a list into two halves.
+# num=[1,2,3,4,56,7,8]
+# mid=len(num)//2
+# first=num[:mid]
+# last=num[mid:]
+# print(first)
+# print(last)
+
+
+# 27. Find the index of a given element without using index().
+# num=[1,2,3,2,4,5,6,7,8]
+# x=int(input("eneter :"))
+# ind=-1
+# for i in range(len(num)):
+#     if num[i]==x:
+#         ind=i
+#         break
+# if ind != -1:
+# # if ind == i:
+#     print(ind)
+# else:
+#     ("elemenet not found")
+
+
+# 28. Write a program to flatten a nested list.
+# num = [[1, 2], [3, 4], [5, 6]]
+# flat=[]
+# for i in num:
+#     # print(i)
+#     for j in i:
+#         flat.append(j)
+#         # print(j)
+# print(flat)
+
+
+# 29. Create a program to find the frequency of each element in a list.
+# nums = [1, 2, 2, 3, 1, 4, 2]
+# for i in nums:
+#     print(i,":",nums.count(i))
+
+
+# 30. Reverse each element of a list of strings.
+# word=['hello','yaseeen','python']
+# result=[]
+# for w in word:
+#     result.append(w[::-1])
+# print(result)
 
 """Section 3: Advanced Level"""
-# 1 31. Implement a matrix using nested lists and print it in matrix format.
-# 2 32. Write a program to add two matrices using nested lists.
-# 3 33. Write a program to transpose a matrix.
-# 4 34. Flatten a 2D list into a single list using list comprehension.
-# 5 35. Find the largest sublist length in a nested list.
-# 6 36. Write a program to find the intersection of multiple lists.
-# 7 37. Write a program to group list elements by their length (strings).
-# 8 38. Implement a simple stack using a Python list.
-# 9 39. Implement a queue using a Python list.
-# 10 40. Write a program to shuffle elements in a list.
-# 11 41. Write a program to find the kth largest element in a list.
-# 12 42. Write a program to check whether a list is a palindrome.
-# 13 43. Write a program to generate all possible pairs from a list.
-# 14 44. Create a list of prime numbers within a given range using list comprehension.
-# 15 45. Write a program to remove all negative numbers from a list.
+# 31. Implement a matrix using nested lists and print it in matrix format.
+# matrix = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
+# for i in matrix:
+#     for j in i:
+#         print(j,end=" ")
+#     print()
+
+
+# 32. Write a program to add two matrices using nested lists.
+
+# 33. Write a program to transpose a matrix.
+# 34. Flatten a 2D list into a single list using list comprehension.
+# 35. Find the largest sublist length in a nested list.
+# 36. Write a program to find the intersection of multiple lists.
+# 37. Write a program to group list elements by their length (strings).
+# 38. Implement a simple stack using a Python list.
+# 39. Implement a queue using a Python list.
+# 40. Write a program to shuffle elements in a list.
+# 41. Write a program to find the kth largest element in a list.
+# 42. Write a program to check whether a list is a palindrome.
+# 43. Write a program to generate all possible pairs from a list.
+# 44. Create a list of prime numbers within a given range using list comprehension.
+# 45. Write a program to remove all negative numbers from a list.
 """# Section 4: Logical / Problem Solving"""
-# 1 46. Given a list of numbers, move all zeros to the end while maintaining order.
-# 2 47. Find the first non-repeating element in a list.
-# 3 48. Find the highest frequency element in a list.
-# 4 49. Find all pairs whose sum equals a given target value.
-# 5 50. Write a program to detect duplicates in a list.
-# 6 51. Given two lists, determine whether they contain the same elements regardless of order.
-# 7 52. Write a program to partition a list into even and odd numbers.
-# 8 53. Implement bubble sort using lists.
-# 9 54. Implement selection sort using lists.
-# 10 55. Write a program to remove consecutive duplicate elements.
+# 46. Given a list of numbers, move all zeros to the end while maintaining order.
+# 47. Find the first non-repeating element in a list.
+# 48. Find the highest frequency element in a list.
+# 49. Find all pairs whose sum equals a given target value.
+# 50. Write a program to detect duplicates in a list.
+# 51. Given two lists, determine whether they contain the same elements regardless of order.
+# 52. Write a program to partition a list into even and odd numbers.
+# 53. Implement bubble sort using lists.
+# 54. Implement selection sort using lists.
+# 55. Write a program to remove consecutive duplicate elements.
 """"# Section 5: Interview-Oriented Questions"""
-# 1 56. Explain the difference between list.copy() and copy.deepcopy(). Demonstrate with code.
-# 2 57. What is list comprehension? Write examples and compare it with loops.
-# 3 58. What happens when you modify a list during iteration? Demonstrate with an example.
-# 4 59. Explain the difference between append(), extend(), and insert().
-# 5 60. Demonstrate shallow copy behavior using lists.
-# 6 61. Explain time complexity of list operations (append, insert, pop).
-# 7 62. Write a program to implement a stack using a list and explain push/pop.
-# 8 63. Write a program to remove duplicates while preserving order.
-# 9 64. Explain the difference between mutable and immutable data structures in Python.
-# 10 65. Write a Python program to simulate a simple task queue using lists.
+# 56. Explain the difference between list.copy() and copy.deepcopy(). Demonstrate with code.
+# 57. What is list comprehension? Write examples and compare it with loops.
+# 58. What happens when you modify a list during iteration? Demonstrate with an example.
+# 59. Explain the difference between append(), extend(), and insert().
+# 60. Demonstrate shallow copy behavior using lists.
+# 61. Explain time complexity of list operations (append, insert, pop).
+# 62. Write a program to implement a stack using a list and explain push/pop.
+# 63. Write a program to remove duplicates while preserving order.
+# 64. Explain the difference between mutable and immutable data structures in Python.
+# 65. Write a Python program to simulate a simple task queue using lists.
