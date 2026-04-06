779ea25ff8514c1cfb94736b23c9ffb34f022560
Muhammed Yaseen <muhammedyaseenporoli@gmail.com>
2026-04-06 16:35:16 +0530

    6 Apr 2026 -tuple work

--- Module_4_Tuple/work_12_tuple.py
+++ Module_4_Tuple/work_12_tuple.py
@@ -141,53 +141,220 @@
 # print(count)
 
 # 23. Find the maximum and minimum values in a tuple of stock prices.
-t=(29,39,39,33,399,398,390,23)
-change=sorted(t)
-minimum=change[0]
-maximum=change[-1]
-print("minimum value :",minimum)
-print('maximum value :',maximum)
+# t=(29,39,39,33,399,398,390,23)
+# change=sorted(t)
+# minimum=change[0]
+# maximum=change[-1]
+# print("minimum value :",minimum)
+# print('maximum value :',maximum)
 
 # 24. Calculate the sum of all numeric elements in a tuple.
+# t=(1,3,4,3,5,'s',23.4)
+# total=0
+# for item in t:
+#     try:
+#         total+=float(item)
+#     except:
+#         pass
+# print('total= ',total)
+
+
+
 # 25. Given a tuple of tuples: ((1, 2), (3, 4), (5, 6)), calculate the sum of the second element of
 # each internal tuple.
+# t=((1, 2), (3, 4), (5, 6))
+# total=0
+# for item in t:
+#     for sec in item[1:]:
+#         total+=sec
+# print(total)
+
+
 # 26. Use the index() method to find the position of 'Apple' starting from index 3 in a large fruit
 # tuple.
+# fruits = ('Banana','Orange','Mango', 'Grapes','Apple','Pineapple','Apple')
+# position = fruits.index('Apple',3)
+# print(position)
+
+
 # 27. Write a function that takes a tuple and returns a new tuple containing only the even
 # numbers.
+# t=(2,3,45,2,4,3,6,7,89,88,76,9)
+# even=[]
+# for i in t:
+#     if i%2==0:
+#         even.append(i)
+# print(tuple(even))
+
+
 # 28. Convert a list of tuples into a single flat list.
+# data=[(1, 2),(3, 4),(5, 6)]
+# flat=[]
+# for i in data:
+#     for j in i:
+#         flat.append(j)
+# print(flat)
+
+
 # 29. Create a tuple from a user-input string where each character is an element.
+# inp=input("enter :")
+# # tup=tuple(inp)
+# # print(tup)
+# result=()
+# for ch in inp:
+#     result+=(ch,)
+# print(result)
+
 # 30. Check if all elements in a tuple are truthy using the all() function.
+# t=(1,2,3,4)
+# if all(t):
+#     print("All elements are truthy")
+# else:
+#     print("Some elements are falsy")
+
+
 """ --- PART 4: SLICING & LOOPING ---"""
 # 31. Extract a sub-tuple containing the elements from index 2 to 5 (inclusive).
+# t = (10, 20, 30, 40, 50, 60, 70)
+# print(t[2:5])
+
+
 # 32. Use slicing to get every second element of a tuple.
+# t = (10, 20, 30, 40, 50, 60, 70)
+# print(t[1::2])
+
+
 # 33. Write a 'for' loop to print each element of a tuple with its corresponding index number.
+# t = (10, 20, 30, 40, 50, 60, 70)
+# for i in range(len(t)):
+#     print("index :",i,"element :",t[i])
+
+
 # 34. Use a 'while' loop to iterate through a tuple backwards.
+# t = (10, 20, 30, 40, 50, 60, 70)
+# len=len(t)-1
+# while len >=0:
+#     print(t[len],end=" ")
+#     len-=1
+
 # 35. Create a tuple of 10 numbers and slice it to get the last 3 elements.
+# t = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# print(t[-3:])
+
 # 36. Slice a tuple to remove the first and last elements.
+# t = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# print(t[1:-1])
+
+
 # 37. Use a for loop to concatenate all strings in a tuple into a single sentence.
-# 38. Compare two tuples (1, 2, 3) and (1, 2, 4). Explain the logic of how Python compares
-# them.
-# 39. Write a program to find duplicate elements in a tuple.
+# t = ("Hello", "this", "is", "a", "sentence")
+# result = ""
+# for w in t:
+#     result+=w+" "
+# print(result)
+
 # 40. Zip two tuples together to create a list of coordinate pairs.
+# t1 = (1, 2, 3)
+# t2 = (4, 5, 6)
+# result = list(zip(t1, t2))
+# print( result)
+
+
 """ PART 5 - Logical Tuple Problems"""
 # 31. Create a tuple of numbers and find the second largest number.
+# t=(23,34,3,53,42,45,56,99,-2)
+# lis=list(t)
+# sort=sorted(lis)
+# print("second laarg :",sort[-2])
+# largest=0
+# second=0
+# # largest=t[0]
+# # second=t[0]
+# for num in t:
+#     if num > largest:
+#         second=largest
+#         largest=num
+#     elif num > second and num != largest:
+#         second=num
+# print(second)
+
+
 # 32. Write a program to remove duplicate elements from a tuple.
+# t=(23,34,3,53,42,45,56,99,2,3,2,3)
+# result=[]
+# for i in t:
+#     if i not in result:
+#         result.append(i)
+# print(result)
+
+
 # 33. Write a program to find the frequency of each element in a tuple.
+# t=(23,34,3,53,42,45,56,99,2,3,2,3)
+# freq=[]
+# for i in t:
+#     if i not in freq:
+#         print(i, ":", t.count(i))
+#         freq.append(i)
+
+
 # 34. Write a program to convert a list of tuples into a dictionary.
 # Example:
 # [(1,'A'), (2,'B'), (3,'C')]
-# 35. Write a program to sort a tuple of numbers.
+
+# lis=[(1,'A'), (2,'B'), (3,'C')]
+# print(dict(lis))
+
+
+
 # 36. Write a program to sort a tuple of tuples based on the second element.
 # Example:
 # ((1,5),(3,2),(4,8))
+# t = ((1, 5), (3, 2), (4, 8))
+# lst = list(t)
+# for i in range(len(lst)):
+#     for j in range(i + 1, len(lst)):
+#         if lst[i][1] > lst[j][1]:
+#             lst[i], lst[j] = lst[j], lst[i]
+# sorted_tuple = tuple(lst)
+# print("Sorted tuple:", sorted_tuple)
+
+
 # 37. Write a program to find common elements between two tuples.
+# t1 =(1,2,3,4,5)
+# t2 =(3,4,5,6,7)
+# common = tuple(set(t1) & set(t2))
+# common = tuple(set(t1).intersection( set(t2)))
+# print("Common :", common)
+
+
 # 38. Write a program to convert a tuple into a string.
+# t = ('y','a','s','e','e','n')
+# result =''.join(t)
+# print("String:", result)
+
 # 39. Write a program to count vowels present in a tuple of characters.
+# t = ('y','a','s','e','e','n')
+# vowel='aeiou'
+# cont=0
+# for ch in t:
+#     if ch in vowel:
+#         cont+=1
+# print(cont)
+
+
 # 40. Write a program to find the product of all numbers in a tuple.
-"""--- PART 6: INDUSTRIAL TRENDS & ADVANCED (MISSING TOPICS)  --- """
+# t =(1,2,3,4,5)
+# product=1
+# for item in t:
+#     product*=item
+# print("Product :",product)
+
+
+"""--- PART 6: INDUSTRIAL TRENDS & ADVANCED (MISSING TOPICS)  --- 
+"""
 # 41. [NamedTuple] Use 'collections.namedtuple' to create a 'Student' object with fields 'name',
 # 'roll_no', and 'course'.
+
 # 42. [Dictionary Keys] Demonstrate using a tuple as a key in a dictionary (e.g.,
 # latitude/longitude). Explain why a list cannot be a key.
 # 43. [Data Integrity] Write a function that returns multiple values (name, age, salary) as a
